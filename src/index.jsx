54f0f3e4899f94bc6e06b0906ad9f8b0c7426f00
emrekaragoz <emrekaragoz.dev@gmail.com>
2025-09-04 10:15:01 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // <-- ekle
 import App from "./App.jsx";
 import { ParcelProvider } from "./contexts/ParcelContext.jsx";
 
@@ -7,7 +8,9 @@ import "./styles.css";
 import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")).render(
-  <ParcelProvider>
-    <App />
-  </ParcelProvider>
+  <BrowserRouter basename="/vparsel">   {/* GitHub Pages alt dizini */}
+    <ParcelProvider>
+      <App />
+    </ParcelProvider>
+  </BrowserRouter>
 );
