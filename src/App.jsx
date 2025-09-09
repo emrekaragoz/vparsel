daa77c3840ffb297a94f1217d3ad3edd14afe401
emrekaragoz <emrekaragoz.dev@gmail.com>
2025-09-09 09:51:55 +0300

    info

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import MapContainer from "./components/Map/MapContainer.jsx";
+import SelectedAreaInfo from "./components/SelectedAreaInfo.jsx";
+import { ParcelProvider, ParcelContext } from "./contexts/ParcelContext.jsx";
+import useParcels from "./hooks/useParcels.js";
+
+function Shell() {
+  const { parcels } = useParcels();
+  const { selectedParcel } = useContext(ParcelContext);
+  const selected = parcels.find((p) => p.id === selectedParcel) || null;
+
+  return (
+    <>
+      <main className="map-wrap">
+        <MapContainer />
+      </main>
+
+      {/* Alt panel */}
+      <SelectedAreaInfo parcel={selected} />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <div className="app-root">
-      {/* Tam ekran harita */}
-      <main className="map-wrap">
-        <MapContainer />
-      </main>
+      <ParcelProvider>
+        <Shell />
+      </ParcelProvider>
     </div>
   );
 }
